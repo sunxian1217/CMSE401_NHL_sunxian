f8acb7e442fe1c4eec6824576029b7daff0717ea
unknown <sunxian@msu.edu>
2023-04-17 22:04:57 -0400

    adding tensorflow opt

--- Project_Part2/tensorflow_opt.py
+++ Project_Part2/tensorflow_opt.py
@@ -1,16 +1,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 import tensorflow as tf
-from sklearn import preprocessing
-from sklearn.datasets import make_classification
-from sklearn.preprocessing import StandardScaler
 
 observations = pd.read_csv('observations.csv')
 
@@ -25,10 +19,7 @@ X = X.astype({'awaywon': 'float32', 'awaylost': 'float32', 'homewon': 'float32',
               'HomeTeamMoneyLine': 'float32', 'PointSpread': 'float32', 'OverUnder': 'float32', 'PointSpreadAwayTeamMoneyLine': 'float32',
               'PointSpreadHomeTeamMoneyLine': 'float32','HomeRotationNumber': 'float32', 'AwayRotationNumber': 'float32', 'OverPayout': 'float32',
               'UnderPayout':'float32'})
-X_train = X[:1000]
-X_test = X[1000:]
-y_train = y[:1000]
-y_test = y[1000:]
+
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
